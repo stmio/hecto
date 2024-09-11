1cb9c2d915625c9248d05b0e7b3366f1a437c53d
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-09-11 17:31:51 +0100

    Add foundation for REPL

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
+#![warn(clippy::all, clippy::pedantic)]
+
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-
-                if c.is_control() {
-                    println!("Binary: {0:08b}\tASCII: {0:#03}\r", b)
-                } else {
-                    println!("Binary: {0:08b}\tASCII: {0:#03}\tCharacter: {1:#?}\r", b, c)
-                }
-
-                if c == 'q' {
-                    break;
-                }
-            }
-            Err(e) => println!("Error: {}", e),
-        }
-    }
-    disable_raw_mode().unwrap();
+    let editor = Editor::default();
+    editor.run();
 }
